003cfb69394d1e0eb3db27632f2f1a2f50ef08db
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-01-26 20:37:21 -0300

    compilando com typescript

---
+++ src/server.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(5, 3);
+console.log("O resultado da soma \u00E9: ".concat(result));
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+function sum(a:number, b:number):number{
+    return a + b
+}
+
+var result = sum(5,3)
+
+console.log(`O resultado da soma é: ${result}`)
