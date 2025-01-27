3d70cef854e7876f5cf1e2a70e295ea47613969a
Dev Pablo H <pablohenriqueadm94@gmail.com>
2025-01-27 13:36:20 -0300

    Query params

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ const PORT = 3333
 
 const app = express() //inicializando o express e colocando dentro da constante app todos os recursos que temos dentro do express
 
-app.get("/products/:id/:user", (request, response)=>{
-    const {id, user} = request.params // recuperando os parametrôs da rota, utilizando express.
-    response.send(`Produto ${id} do usuário ${user}`) 
+app.get("/products", (request, response)=>{
+    // /products?page=1&limit=10
+    const {page,limit} = request.query
+    response.send(`Página ${page} de ${limit}`) 
 })
 
 app.listen(PORT, ()=> console.log(`Server is running on at ${PORT}`)) // definindo um número da porta da aplicação.
