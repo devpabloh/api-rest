85a49c52bf633b826a869007aedee70bea43f28d
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-01-27 11:16:52 -0300

    Enviando requisições com o método GET

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,9 @@ const PORT = 3333
 
 const app = express() //inicializando o express e colocando dentro da constante app todos os recursos que temos dentro do express
 
-app.listen(PORT, ()=> console.log(`Server is running on at ${PORT}`)) // definindo a porta da API
+app.get("/", (request, response)=>{
+    response.send("Hello word Express!")
+})
+
+app.listen(PORT, ()=> console.log(`Server is running on at ${PORT}`)) // definindo um número da porta da aplicação.
 
