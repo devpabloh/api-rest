6fa2156ff12027fa016d05fd14a03dbac36bce3b
Dev Pablo H <pablohenriqueadm94@gmail.com>
2025-01-27 13:54:57 -0300

    Dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,17 @@ const PORT = 3333
 
 const app = express() //inicializando o express e colocando dentro da constante app todos os recursos que temos dentro do express
 
+app.use(express.json()) // Aqui estamos dizendo para API qual tipo de estrutura vamos utilizar json,xml, etc.
+
 app.get("/products", (request, response)=>{
     // /products?page=1&limit=10
     const {page,limit} = request.query
     response.send(`Página ${page} de ${limit}`) 
+})
+
+app.post("/products", (request, response)=>{
+    const {name, price} = request.body // Aqui estamos recuperando de acordo com a requisição feita no no body do post (name, price)
+    response.send(`Produto ${name}, custa ${price}`)
 })
 
 app.listen(PORT, ()=> console.log(`Server is running on at ${PORT}`)) // definindo um número da porta da aplicação.
