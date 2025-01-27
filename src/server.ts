6439f85e201e54d0f62f33d11ae21d5677b86de9
Dev Pablo H <pablohenriqueadm94@gmail.com>
2025-01-27 13:27:50 -0300

    route.params

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ const PORT = 3333
 
 const app = express() //inicializando o express e colocando dentro da constante app todos os recursos que temos dentro do express
 
-app.get("/", (request, response)=>{
-    response.send("Hello word Express!")
+app.get("/products/:id/:user", (request, response)=>{
+    const {id, user} = request.params // recuperando os parametrôs da rota, utilizando express.
+    response.send(`Produto ${id} do usuário ${user}`) 
 })
 
 app.listen(PORT, ()=> console.log(`Server is running on at ${PORT}`)) // definindo um número da porta da aplicação.
